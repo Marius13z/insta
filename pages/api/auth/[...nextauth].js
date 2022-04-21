dd043a5dcfb58f4a17d02db2c524507429eb1db3
Marius13z <97192902+Marius13z@users.noreply.github.com>
2022-04-21 19:37:29 +0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,9 +8,8 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    secret:process.env.SECRET,
   ],
-  secret:process.env.SECRET,
   pages: {
       signIn:"/auth/signin",    
   },
